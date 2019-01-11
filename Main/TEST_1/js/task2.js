35cee430d335bb54356a054ad391384e1a4c7ac7
porsh99 <kladovlesha@mail.ru>
2019-01-11 15:41:51 +0300

    test-1 ver2 beta

--- Main/TEST_1/js/task2.js
+++ Main/TEST_1/js/task2.js
@@ -14,11 +14,16 @@ var chatImageSource = "img/car1.jpg";
 
 
 
+var parentMessageContainerName = "chat-messages-container";
+
+
+
 
 function generateChats() {
 
 	for (var i = 0; i < 10; i++) {
 		generateChat(parentContainerIDName);
+		generateChatMessages(parentMessageContainerName);
 	}
 }
 
@@ -28,35 +33,35 @@ function generateChat(parentContainerID) {
 	//console.log(parentContainer);
 
 	var headerText = document.createElement("text");
-    var lastMessageText = document.createElement("text");
-    var chatImage = document.createElement("img");
+	var lastMessageText = document.createElement("text");
+	var chatImage = document.createElement("img");
 
 	var chat = document.createElement("div");
 
 	var divImageBox = document.createElement("div");
-    var divChatTextBox = document.createElement("div");
-    
+	var divChatTextBox = document.createElement("div");
+
 	var divChatHeader = document.createElement("div");
 	var divChatLastMessage = document.createElement("div");
 
 
 	chat.setAttribute("class", chatClassName);
 
-    divImageBox.setAttribute("class", divImageBoxClassName);
-    divChatTextBox.setAttribute("class", divTextBoxClassName);
-    chatImage.setAttribute("src", chatImageSource);
+	divImageBox.setAttribute("class", divImageBoxClassName);
+	divChatTextBox.setAttribute("class", divTextBoxClassName);
+	chatImage.setAttribute("src", chatImageSource);
 
-    divChatHeader.setAttribute("class", chatHeaderClassName);
-    divChatLastMessage.setAttribute("class", chatLastMessageClassName);
+	divChatHeader.setAttribute("class", chatHeaderClassName);
+	divChatLastMessage.setAttribute("class", chatLastMessageClassName);
 
 	divChatHeader.appendChild(headerText);
-    divChatLastMessage.appendChild(lastMessageText);
-    
-    divChatTextBox.appendChild(divChatHeader);
-    divChatTextBox.appendChild(divChatLastMessage);
+	divChatLastMessage.appendChild(lastMessageText);
 
-    divImageBox.appendChild(chatImage);
-    
+	divChatTextBox.appendChild(divChatHeader);
+	divChatTextBox.appendChild(divChatLastMessage);
+
+	divImageBox.appendChild(chatImage);
+
 
 	chat.appendChild(divImageBox);
 	chat.appendChild(divChatTextBox);
@@ -65,4 +70,49 @@ function generateChat(parentContainerID) {
 
 	headerText.outerHTML = headerTextContent;
 	lastMessageText.outerHTML = lastMessageContent;
+}
+
+function generateChatMessages(parentContainerID) {
+	var parentContainer = document.getElementById(parentContainerID);
+
+	var divMessageBox = document.createElement("div");
+	var divMessageImageBox = document.createElement("div");
+	var divTryangleBox = document.createElement("div");
+	var divMessageTextBox = document.createElement("div");
+
+	var messageImage = document.createElement("img");
+	var tryangleContainer = document.createElement("svg");
+	var messageText = document.createElement("text");
+
+	divMessageBox.setAttribute("class", "message-box");
+	divMessageImageBox.setAttribute("class", "message-image-box");
+	divTryangleBox.setAttribute("class", "triangle-container");
+	divMessageTextBox.setAttribute("class", "message-text-box");
+
+	var poligon = document.createElement("polygon");
+	poligon.setAttribute("class", "message-box");
+
+	poligon.setAttribute("points", "13,0 0,10 13,10");
+	tryangleContainer.appendChild(poligon);
+	divTryangleBox.appendChild(tryangleContainer);
+
+	messageImage.setAttribute("src", "img/car3.jpg");
+	divMessageImageBox.appendChild(messageImage);
+
+	divMessageTextBox.appendChild(messageText);
+
+
+	divMessageBox.appendChild(divMessageImageBox);
+	divMessageBox.appendChild(divTryangleBox);
+	divMessageBox.appendChild(divMessageTextBox);
+
+	
+
+	parentContainer.appendChild(divMessageBox);
+
+	messageText.outerHTML = " Lorem ipsum sit amet consectetur adipsiting elit. Sed magni expedita non in dolor recusandae,repellat officia at, fugit laboriosam incidunt desurant perspicitiantis sint atque eius decimusplaceat necessitatibus coluptatem.";
+
+
+
+
 }
