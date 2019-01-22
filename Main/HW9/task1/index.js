3db1adbeb1aece55962e0bb660283ae7874836f1
porsh99 <kladovlesha@mail.ru>
2019-01-22 15:08:38 +0300

    HW9 task-1 step-6

--- Main/HW9/task1/index.js
+++ Main/HW9/task1/index.js
@@ -1,8 +1,8 @@
 sap.ui.require([
 	"sap/ui/model/json/JSONModel",
     "sap/ui/core/mvc/XMLView",
-    "sap/ui/model/BindingMode"
-], function (JSONModel, XMLView, BindingMode) {
+    "sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
 	sap.ui.getCore().attachInit(function () {
@@ -11,14 +11,16 @@ sap.ui.require([
 			firstName: "Kladov",
 			lastName: "Alexey",
 			enabled: true,
-			panelHeaderText: "Data Binding Basics"
-
         });
-        
-        oModel.setDefaultBindingMode(BindingMode.OneWay);
 
         sap.ui.getCore().setModel(oModel);
         
+        var oResourceModel = new ResourceModel({
+			bundleName: "sap.ui.demo.db.i18n.i18n"
+		});
+
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
+
 		new XMLView({
 			viewName: "sap.ui.demo.db.view.App"
 		}).placeAt("content");
