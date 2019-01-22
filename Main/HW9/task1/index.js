63c36c59253f6b9dea198b563bcc30ed62672c47
porsh99 <kladovlesha@mail.ru>
2019-01-22 14:24:51 +0300

    HW9 task-1 step-2

--- Main/HW9/task1/index.js
+++ Main/HW9/task1/index.js
@@ -1,9 +1,17 @@
 sap.ui.require([
-	"sap/m/Text"
-], function (Text) {
+    "sap/m/Text",
+    "sap/ui/model/json/JSONModel"
+], function (Text, JSONModel) {
 	"use strict";
 
 	sap.ui.getCore().attachInit(function () {
+
+		var oModel = new JSONModel({
+			greetingText: "Hi, my name is Harry Hawk"
+		});
+
+		sap.ui.getCore().setModel(oModel);
+
 		new Text({text: "Hi, my name is Harry Hawk"}).placeAt("content");
 	});
 });
