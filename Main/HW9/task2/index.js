c540a490aad976c135dce331db3a598ab31b3c46
porsh99 <kladovlesha@mail.ru>
2019-01-23 14:50:24 +0300

    hw9 task-2 ver2 need refactoring

--- Main/HW9/task2/index.js
+++ Main/HW9/task2/index.js
@@ -7,12 +7,16 @@ sap.ui.require([
 
 	sap.ui.getCore().attachInit(function () {
 
-		var oModel = new JSONModel();
-		oModel.setData(getInfo());
+		// var oModel = new JSONModel();
+		// oModel.setData(getInfo());
 
-		console.log(oModel);
+		var url2 = "https://services.odata.org/TripPinRESTierService/(S(3mvqq3yavajb5ek4qtiqx1js))/People?$select=FirstName,LastName,Gender";
+		var oModel2 = new JSONModel(url2);
 
-		sap.ui.getCore().setModel(oModel, "products");
+		// console.log(oModel);
+		console.log(oModel2);
+
+		sap.ui.getCore().setModel(oModel2, "products");
 	
 		var oResourceModel = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n"
@@ -27,24 +31,25 @@ sap.ui.require([
 		oView.placeAt("content");
 
 
-		function getInfo() {
+		// function getInfo() {
 
-			var pr = {Products: data}
+		// 	var pr = {Products: data}
 			
-			var data = [];
-			$.ajax({
-				type: "get",
-				async: false,
-				url: "https://services.odata.org/TripPinRESTierService/(S(3mvqq3yavajb5ek4qtiqx1js))/People?$select=FirstName,LastName,Gender",
-				success: function (_data) {
-					data = _data.value;
-				},
-				error: function (xhr, textStatus, errorMessage) {}
-			});
+		// 	var data = [];
+		// 	$.ajax({
+		// 		type: "get",
+		// 		async: false,
+		// 		url: "https://services.odata.org/TripPinRESTierService/(S(3mvqq3yavajb5ek4qtiqx1js))/People?$select=FirstName,LastName,Gender",
+		// 		success: function (_data) {
+		// 			data = _data.value;
+		// 		},
+		// 		error: function (xhr, textStatus, errorMessage) {}
+		// 	});
 
-			pr.Products = data;
-			return pr;
-		}
+		// 	pr.Products = data;
+		// 	return pr;
+
+		// }
 
 	});
 });
